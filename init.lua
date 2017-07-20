a8f16467bc2a68808c136df8c12611de176d6d14
wangxingyou [王星友] <wangxingyou@xiaomi.com>
2017-07-20 11:32:30 +0800

    change keybinding
    Signed-off-by: wangxingyou [王星友] <wangxingyou@xiaomi.com>

--- init.lua
+++ init.lua
@@ -300,23 +300,23 @@ function resize_win(direction)
     end
 end
 
-resizeextra_lefthalf_keys = resizeextra_lefthalf_keys or {{"cmd", "alt"}, "left"}
+resizeextra_lefthalf_keys = resizeextra_lefthalf_keys or {{"ctrl", "cmd", "alt"}, "left"}
 if string.len(resizeextra_lefthalf_keys[2]) > 0 then
     hs.hotkey.bind(resizeextra_lefthalf_keys[1], resizeextra_lefthalf_keys[2], "Lefthalf of Screen", function() resize_win('halfleft') end)
 end
-resizeextra_righthalf_keys = resizeextra_righthalf_keys or {{"cmd", "alt"}, "right"}
+resizeextra_righthalf_keys = resizeextra_righthalf_keys or {{"ctrl", "cmd", "alt"}, "right"}
 if string.len(resizeextra_righthalf_keys[2]) > 0 then
     hs.hotkey.bind(resizeextra_righthalf_keys[1], resizeextra_righthalf_keys[2], "Righthalf of Screen", function() resize_win('halfright') end)
 end
-resizeextra_fullscreen_keys = resizeextra_fullscreen_keys or {{"cmd", "alt"}, "up"}
+resizeextra_fullscreen_keys = resizeextra_fullscreen_keys or {{"ctrl", "cmd", "alt"}, "up"}
 if string.len(resizeextra_fullscreen_keys[2]) > 0 then
     hs.hotkey.bind(resizeextra_fullscreen_keys[1], resizeextra_fullscreen_keys[2], "Fullscreen", function() resize_win('fullscreen') end)
 end
-resizeextra_fcenter_keys = resizeextra_fcenter_keys or {{"cmd", "alt"}, "down"}
+resizeextra_fcenter_keys = resizeextra_fcenter_keys or {{"ctrl", "cmd", "alt"}, "down"}
 if string.len(resizeextra_fcenter_keys[2]) > 0 then
     hs.hotkey.bind(resizeextra_fcenter_keys[1], resizeextra_fcenter_keys[2], "Resize & Center", function() resize_win('fcenter') end)
 end
-resizeextra_center_keys = resizeextra_center_keys or {{"cmd", "alt"}, "return"}
+resizeextra_center_keys = resizeextra_center_keys or {{"ctrl", "cmd", "alt"}, "return"}
 if string.len(resizeextra_center_keys[2]) > 0 then
     hs.hotkey.bind(resizeextra_center_keys[1], resizeextra_center_keys[2], "Center Window", function() resize_win('center') end)
 end
